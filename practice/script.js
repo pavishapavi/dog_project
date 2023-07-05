48bfd96933891c5d751e1bab1fb8cd33d7099e9d
Anusuya-Balakrishnan <anusuyabalakrishnan01@gmail.com>
2023-07-05 11:06:09 +0530

    image size change

---
+++ practice/script.js
@@ -0,0 +1,72 @@
+// 10;
+// 10.5;
+
+// ("text");
+// "text"`text`;
+
+// true or false
+
+// ["testing", "help"];
+
+// {name:'python',age:20}
+
+// let name = "javascript";
+// let age = 10;
+// console.log(`my name is ${name} i am ${age} years old`);
+
+// if (age > 18) {
+//   console.log("your elegible for voter id");
+// } else if (age == 18) {
+//   console.log("you can apply for voter id");
+// } else {
+//   console.log("your not elegible for voter id");
+// }
+
+// let num = 50;
+// if (num > 50) {
+//   console.log("it is odd  number");
+// } else if (num == 50) {
+//   console.log("it is even number");
+// } else {
+//   console.log("it is not even number");
+// }
+
+// 05-07-2023
+
+// function funcName(a) {
+//   console.log(a, "named function");
+// }
+
+// funcName("javascript");
+
+// const exprestion = function (name) {
+//   console.log(name, "Expretion function");
+// };
+
+// exprestion("HTML");
+
+// const d = (arg) => {
+//   console.log(arg, "arrow function");
+// };
+// arrow("value");
+
+// alert("this is my msg");
+
+// const p = prompt("Enter your name");
+// console.log(p, " <= this is my input");
+
+// const b = confirm("Do you want continue");
+// console.log(b);
+
+const obj = document.getElementById("h");
+
+function objClick() {
+  console.log(obj);
+  obj.style.color = "red";
+  obj.style.fontSize = "100px";
+}
+const box = document.getElementById("box");
+function boxClick() {
+  console.log(box);
+  box.style.backgroundColor = "red";
+}
